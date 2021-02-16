5c6c93ec0e0e692b005df645782d7dac8ea0ef21
prgmw <paulomedeirosw@gmail.com>
2021-02-16 19:45:25 -0300

    Config para cors

--- src/main/java/br/com/money/cors/CorsFilter.java
+++ src/main/java/br/com/money/cors/CorsFilter.java
@@ -1,54 +1,58 @@
-//package br.com.money.cors;
-//
-//import br.com.money.config.property.MoneyProperty;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.Ordered;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.stereotype.Component;
-//
-//import javax.servlet.*;
-//import javax.servlet.http.HttpServletRequest;lo
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//
-//@Component
-//@Order(Ordered.HIGHEST_PRECEDENCE)
-//public class CorsFilter implements Filter {
-//
+package br.com.money.cors;
+
+import br.com.money.config.property.MoneyProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
 //    @Autowired
 //    private MoneyProperty property;
+
+    @Value("${money.origem.endereco}")
+    private String origin;
 //
 //    private String origemPermitida = property.getOrigem().getEndereco();
-//
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//
-//    }
-//
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//
-//        HttpServletRequest req =  (HttpServletRequest) request;
-//        HttpServletResponse resp =  (HttpServletResponse) response;
-//
-//        resp.setHeader("Access-Control-Allow-Origin", origemPermitida);
-//        resp.setHeader("Access-Control-Allow-Credentials", "true");
-//
-//        if("OPTIONS".equals(req.getMethod()) && origemPermitida.equals(req.getHeader("Origin"))) {
-//            resp.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
-//            resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-//            resp.setHeader("Access-Control-Max-Age", "3600");
-//
-//            resp.setStatus(HttpServletResponse.SC_OK);
-//
-//        } else {
-//            chain.doFilter(req, resp);
-//        }
-//    }
-//
-//    @Override
-//    public void destroy() {
-//
-//    }
-//}
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req =  (HttpServletRequest) request;
+        HttpServletResponse resp =  (HttpServletResponse) response;
+
+        resp.setHeader("Access-Control-Allow-Origin", origin);
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if("OPTIONS".equals(req.getMethod()) && origin.equals(req.getHeader("Origin"))) {
+            resp.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            resp.setHeader("Access-Control-Max-Age", "3600");
+
+            resp.setStatus(HttpServletResponse.SC_OK);
+
+        } else {
+            chain.doFilter(req, resp);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
